ccc13598bb49002a8e9f0ec9944b3f3807d8588f
John Giorshev <johngiorshev@gmail.com>
2025-10-19 11:00:17 -0400

    mut

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,24 @@
 macro_rules! world {
     // https://stackoverflow.com/a/37754096/15534181
     //
-    // main entry: generate one visit_* function per trait
+    // generate visit_* functions per trait
     (@pass_entity_tuple $($trait_name:ident),* @ $entity_tuple:tt) => {
         paste::paste! {
             $(
-                // don't suppress unused here - it means we have generated a
-                // query on a trait which isn't being used anywhere - useful to
-                // known
-                pub fn [<visit_ $trait_name:snake>]<F>(&mut self, mut handler: F)
+                #[allow(unused)]
+                pub fn [<visit_ $trait_name:snake>]<F>(&self, mut handler: F)
+                where
+                    F: FnMut(&dyn $trait_name)
+                {
+                    world!(@use_entity_tuple $trait_name $entity_tuple self handler);
+                }
+
+                #[allow(unused)]
+                pub fn [<visit_mut_ $trait_name:snake>]<F>(&mut self, mut handler: F)
                 where
                     F: FnMut(&mut dyn $trait_name)
                 {
-                    world!(@use_entity_tuple $trait_name $entity_tuple self handler);
+                    world!(@use_entity_tuple_mut $trait_name $entity_tuple self handler);
                 }
             )*
         }
@@ -28,6 +34,17 @@ macro_rules! world {
         paste::paste! {
             $(
                 <() as [<VisitIf $trait_name>]<$entity>>::visit_if_applicable(
+                    &$self_ident.[<$entity:snake>],
+                    &mut $handler_ident,
+                );
+            )*
+        }
+    };
+
+    (@use_entity_tuple_mut $trait_name:ident ($( $entity:ty ),*) $self_ident:ident $handler_ident:ident) => {
+        paste::paste! {
+            $(
+                <() as [<VisitIf $trait_name>]<$entity>>::visit_if_applicable_mut(
                     &mut $self_ident.[<$entity:snake>],
                     &mut $handler_ident,
                 );
@@ -55,24 +72,40 @@ pub struct $struct_name {
 }
 
 $(
-    // don't suppress unused here for same reason as above
     trait [<VisitIf $trait_name>]<T> {
-        fn visit_if_applicable<F>(arena: &mut generational_arena::Arena<T>, handler: F)
+        fn visit_if_applicable<F>(arena: &generational_arena::Arena<T>, handler: F)
+        where
+            F: FnMut(&dyn $trait_name);
+
+        fn visit_if_applicable_mut<F>(arena: &mut generational_arena::Arena<T>, handler: F)
         where
             F: FnMut(&mut dyn $trait_name);
     }
 
+    // no-op for types not implementing the trait
     impl<T> [<VisitIf $trait_name>]<T> for () {
-        default fn visit_if_applicable<F>(_arena: &mut generational_arena::Arena<T>, _handler: F)
+        default fn visit_if_applicable<F>(_arena: &generational_arena::Arena<T>, _handler: F)
+        where
+            F: FnMut(&dyn $trait_name),
+        {}
+
+        default fn visit_if_applicable_mut<F>(_arena: &mut generational_arena::Arena<T>, _handler: F)
         where
             F: FnMut(&mut dyn $trait_name),
-        {
-            // default: no-op (for types that DO NOT implement $trait_name)
-        }
+        {}
     }
 
     impl<T: $trait_name> [<VisitIf $trait_name>]<T> for () {
-        fn visit_if_applicable<F>(arena: &mut generational_arena::Arena<T>, mut handler: F)
+        fn visit_if_applicable<F>(arena: &generational_arena::Arena<T>, mut handler: F)
+        where
+            F: FnMut(&dyn $trait_name),
+        {
+            for (_idx, entity) in arena.iter() {
+                handler(entity as &dyn $trait_name);
+            }
+        }
+
+        fn visit_if_applicable_mut<F>(arena: &mut generational_arena::Arena<T>, mut handler: F)
         where
             F: FnMut(&mut dyn $trait_name),
         {
@@ -84,14 +117,18 @@ $(
 )*
 
 impl $struct_name {
-    // pass all traits and entities to generate visit_* methods
     $crate::world!(@pass_entity_tuple $($trait_name),* @ ($($entity),*));
+
+
 }
 
         }
     };
 }
 
+// ---------------------------------------------------------------
+// Tests
+// ---------------------------------------------------------------
 #[cfg(test)]
 mod tests {
     #[derive(Debug)]
@@ -129,7 +166,8 @@ mod tests {
         }
     }
 
-    // name of world struct, contained entity types, contained entity traits
+    // name of world struct, contained entity types, contained entity traits.
+    // entity types should be 'static
     world!(MyWorld, Player, Enemy; TestTrait, SecondTestTrait);
 
     #[test]
@@ -139,6 +177,6 @@ mod tests {
         world.player.insert(Player { id: 1 });
         world.enemy.insert(Enemy { hp: 10 });
         world.visit_test_trait(|e| e.do_something());
-        world.visit_second_test_trait(|e| e.do_something_else());
+        world.visit_mut_second_test_trait(|e| e.do_something_else());
     }
 }
